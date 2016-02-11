df0291c03024b4138ffbddfcf2339781181749c1
Ryan Yue <yue.ryan123@gmail.com>
2016-02-10 22:17:15 -0800

    first commit

---
+++ Yelpify/Yelpify/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Yelpify
+//
+//  Created by Ryan Yue on 2/10/16.
+//  Copyright © 2016 Yelpify. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
