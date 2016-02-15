2f269c3e154ba423370a4a652eec840512befc15
Ryan Yue <yue.ryan123@gmail.com>
2016-02-15 14:06:43 -0800

    test

--- Yelpify/Yelpify/PlaylistViewController.swift
+++ Yelpify/Yelpify/PlaylistViewController.swift
@@ -12,6 +12,7 @@ import ParseUI
 
 class PlaylistViewController: UICollectionViewController, PFLogInViewControllerDelegate, PFSignUpViewControllerDelegate {
 
+    // SDFJSDKFSDF
     private let reuseIdentifier = "PlaylistCell"
     private let sectionInsets = UIEdgeInsets(top: 50.0, left: 20.0, bottom: 50.0, right: 20.0)
     override func viewDidLoad() {
