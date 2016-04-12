a061b2f14d172746466e9f7bcc7382398c79192c
Jonathan Lam <jonathan.lam97@gmail.com>
2016-04-12 15:11:06 -0700

    actionsMenuVC

--- Yelpify/Yelpify/ActionsViewController.swift
+++ Yelpify/Yelpify/ActionsViewController.swift
@@ -11,13 +11,47 @@ import UIKit
 class ActionsViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, UIScrollViewDelegate {
     
     @IBOutlet weak var tableView: UITableView!
-    
+    @IBOutlet weak var tableHeaderView: UIView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        configureHeaderView()
+        applyBackgroundBlurEffect()
         view.backgroundColor = UIColor.clearColor()
         view.opaque = false
+    
     }
+    
+    func applyBackgroundBlurEffect() {
+        // Blur Effect
+        let blurEffect = UIBlurEffect(style: UIBlurEffectStyle.Dark)
+        let blurEffectView = UIVisualEffectView(effect: blurEffect)
+        blurEffectView.frame = self.view.bounds
+        
+        // Vibrancy Effect
+        let vibrancyEffect = UIVibrancyEffect(forBlurEffect: blurEffect)
+        let vibrancyEffectView = UIVisualEffectView(effect: vibrancyEffect)
+        vibrancyEffectView.frame = blurEffectView.bounds
+        
+        // Add to subview
+        view.addSubview(blurEffectView)
+        view.addSubview(vibrancyEffectView)
+    }
+    
+    func configureHeaderView(){
+        var headerRect = CGRect(x: 0, y: 0, width: tableView.frame.size.width, height: self.view.frame.size.height * 0.5)
+        tableHeaderView.frame = headerRect
+
+    }
+    
+    func scrollViewDidScroll(scrollView: UIScrollView) {
+        if tableView.contentOffset.y < -100{
+            performSegueWithIdentifier("unwindToSinglePlaylist", sender: self)
+        }
+    }
+    
+    
+    // MARK: - Table View Functions
 
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         var cell = tableView.dequeueReusableCellWithIdentifier("actionCell", forIndexPath: indexPath)
@@ -26,7 +60,7 @@ class ActionsViewController: UIViewController, UITableViewDelegate, UITableViewD
     }
     
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
+        return 6
     }
     
     func numberOfSectionsInTableView(tableView: UITableView) -> Int {
