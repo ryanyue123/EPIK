259c83713fa857c0ed36d7b363898f193526be62
Jonathan Lam <jonathan.lam97@gmail.com>
2016-04-11 17:47:36 -0700

    randomBusinessVC UI

--- Yelpify/Yelpify/RandomPlaceController.swift
+++ Yelpify/Yelpify/RandomPlaceController.swift
@@ -19,21 +19,38 @@ class RandomPlaceController: UIViewController{
     @IBOutlet weak var RestaurantName: UILabel!
     @IBOutlet weak var RestaurantAddress: UILabel!
     
+    @IBOutlet weak var backgroundImage: UIImageView!
+    @IBOutlet weak var cardImageView: UIImageView!
     
     func getRandomPlace(playlist: [Business])->Business{
         let num = Int32(arc4random_uniform(UInt32(playlist.count)))
         let randomPlace = playlist[Int(num)]
         return randomPlace
-        
     }
     
-    //
+    func applyBackgroundBlurEffect() {
+        // Blur Effect
+        let blurEffect = UIBlurEffect(style: UIBlurEffectStyle.Dark)
+        let blurEffectView = UIVisualEffectView(effect: blurEffect)
+        blurEffectView.frame = self.view.bounds
+        
+        // Vibrancy Effect
+        let vibrancyEffect = UIVibrancyEffect(forBlurEffect: blurEffect)
+        let vibrancyEffectView = UIVisualEffectView(effect: vibrancyEffect)
+        vibrancyEffectView.frame = blurEffectView.bounds
+        
+        // Add to subview
+        backgroundImage.addSubview(blurEffectView)
+        backgroundImage.addSubview(vibrancyEffectView)
+    }
+
     
     override func viewDidLoad() {
         let randomBusiness = getRandomPlace([business1, business2, business2])
         RestaurantName.text = randomBusiness.businessName
         RestaurantAddress.text = randomBusiness.businessAddress
-
+        applyBackgroundBlurEffect()
+        cardImageView.layer.cornerRadius = 30.0
     }
     
    //MARK: Actions
