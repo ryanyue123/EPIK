2996dcd4b0cb30ef876da7883051f226467657b8
Ryan Yue <yue.ryan123@gmail.com>
2016-04-29 17:47:34 -0700

    Search playlist by name

--- Yelpify/Yelpify/SearchPlaylistViewController.swift
+++ Yelpify/Yelpify/SearchPlaylistViewController.swift
@@ -7,18 +7,14 @@
 //
 
 import UIKit
-
-class SearchPlaylistViewController: UITableViewController {
+import Parse
+class SearchPlaylistViewController: UITableViewController, UITextFieldDelegate {
 
     @IBOutlet weak var textField: UITextField!
+    var playlist_query = [PFObject]()
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+        textField.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
@@ -30,67 +26,39 @@ class SearchPlaylistViewController: UITableViewController {
 
     override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return self.playlist_query.count
     }
-
-    /*
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        searchForPlaylistWithName()
+        self.textField.endEditing(true)
+        return true
+    }
+    
+    func searchForPlaylistWithName()
+    {
+        let query = PFQuery(className: "Playlists")
+        query.whereKey("playlistName", containsString: self.textField.text!)
+        query.findObjectsInBackgroundWithBlock {(objects: [PFObject]?, error: NSError?) -> Void in
+            if (error == nil)
+            {
+                dispatch_async(dispatch_get_main_queue(), {
+                    self.playlist_query = objects!
+                    self.tableView.reloadData()
+                    print("dslkjflksd")
+                })
+            }
+        }
+    }
+    
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCellWithIdentifier("reuseIdentifier", forIndexPath: indexPath)
-
-        // Configure the cell...
+        let cell = tableView.dequeueReusableCellWithIdentifier("cell", forIndexPath: indexPath) as! SearchPlaylistCell
+        cell.label.text = playlist_query[indexPath.row]["playlistName"] as? String
 
         return cell
     }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
-        if editingStyle == .Delete {
-            // Delete the row from the data source
-            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
-        } else if editingStyle == .Insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(tableView: UITableView, moveRowAtIndexPath fromIndexPath: NSIndexPath, toIndexPath: NSIndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
